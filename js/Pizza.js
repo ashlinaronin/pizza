820dd4e1fd6e3aff144206d17f3086890fec246d
Ashlin <ashlinaronin@gmail.com>
2015-09-18 10:45:03 -0700

    Pass cheese tests

--- js/Pizza.js
+++ js/Pizza.js
@@ -11,5 +11,29 @@ function Pizza(size, toppings, glutenFree, vegan) {
 };
 
 Pizza.prototype.calculatePrice = function() {
+    // Base prices
+    var basePrice = 0.0;
+    console.log("this.size is " + this.size);
 
+    switch (this.size) {
+        case "small":
+            console.log("case small");
+            basePrice = 11.99;
+            break;
+        case "medium":
+            basePrice = 13.99;
+            break;
+        case "large":
+            basePrice = 19.99;
+            break;
+        case "xlarge":
+            basePrice = 24.99;
+            break;
+        case "xxxlarge":
+            basePrice = 34.99;
+            break;
+    }
+
+    this.price = basePrice;
+    return basePrice;
 };
